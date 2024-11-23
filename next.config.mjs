79c2a5bd641359e4beb0a384c2b3faf6c20db41f
Jitendra39 <159651421+Jitendra39@users.noreply.github.com>
2024-11-24 00:15:43 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud'
+        hostname: 'lovely-flamingo-139.convex.cloud',
       },
       {
         protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
+        hostname: 'sleek-capybara-771.convex.cloud',
       },
       {
         protocol: 'https',
-        hostname: 'img.clerk.com'
+        hostname: 'img.clerk.com',
       },
-    ]
-  }
+      // Allow all protocols and hostnames
+      {
+        protocol: '**', // Matches any protocol (http, https, etc.)
+        hostname: '**', // Matches any domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
