fd6eec1fe891b7003f1e9a1382bb9318b4684d79
Jitendra39 <159651421+Jitendra39@users.noreply.github.com>
2024-11-23 16:29:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.clerk.com",
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
       },
       {
-        protocol: "https",
-        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        protocol: 'https',
+        hostname: 'sleek-capybara-771.convex.cloud'
       },
       {
-        protocol: "https",
-        hostname: "lovely-flamingo-139.convex.cloud",
+        protocol: 'https',
+        hostname: 'img.clerk.com'
       },
-      {
-        protocol: "https",
-        hostname: "notable-viper-311.convex.cloud",
-      },
-      {
-        protocol: "https",
-        hostname: "sleek-capybara-771.convex.cloud",
-      },
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-    ],
-  },
+    ]
+  }
 };
 
 export default nextConfig;
