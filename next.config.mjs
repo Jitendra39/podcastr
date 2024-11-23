ce87156e8510978d32b1c7d08e39b6dfd541f68e
Jitendra39 <159651421+Jitendra39@users.noreply.github.com>
2024-11-24 01:02:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,28 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud',
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
       {
-        protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud',
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
       },
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "notable-viper-311.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "sleek-capybara-771.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
     ],
   },
