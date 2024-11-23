dda631ea385ef2edba721f4031284a58198ed20e
Jitendra39 <159651421+Jitendra39@users.noreply.github.com>
2024-11-24 00:53:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,11 +17,6 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'img.clerk.com',
       },
-      // Allow all protocols and hostnames
-      {
-        protocol: '**', // Matches any protocol (http, https, etc.)
-        hostname: '**', // Matches any domain
-      },
     ],
   },
 };
